ee5f514aa348fe75f334acf65858b391a4a701b0
pandawaa <pallakangoding@gmail.com>
2025-01-24 07:15:29 +0800

    refactor types ts

--- types.d.ts
+++ types.d.ts
@@ -1,19 +1,35 @@
+// interface Book {
+//    id: string;
+//    title: string;
+//    author: string;
+//    genre: string;
+//    rating: number;
+//    totalCopies: number;
+//    availableCopies: number;
+//    description: string;
+//    coverColor: string;
+//    coverUrl: string;
+//    videoUrl: string;
+//    summary: string;
+//    createdAt: Date | null;
+//  }
+
 interface Book {
-   id: string;
-   title: string;
-   author: string;
-   genre: string;
-   rating: number;
-   totalCopies: number;
-   availableCopies: number;
-   description: string;
-   coverColor: string;
-   coverUrl: string;
-   videoUrl: string;
-   summary: string;
-   createdAt: Date | null;
- }
- 
+  id: number;
+  title: string;
+  author: string;
+  genre: string;
+  rating: number;
+  total_copies: number;
+  available_copies: number;
+  description: string;
+  color: string;
+  cover: string;
+  video: string;
+  summary: string;
+}
+
+
  interface AuthCredentials {
    fullName: string;
    email: string;
